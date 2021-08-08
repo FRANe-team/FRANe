18bce3ffad968a29015ecf8b2545dc3bc541e896
urh <urh.primozic@gmail.com>
2021-08-08 10:39:13 +0200

    ready for PyPI

---
+++ frane/__init__.py
@@ -0,0 +1,9 @@
+'''
+FRANe
+
+Unsupervised Feature Ranking via Attribute Networks. 
+'''
+
+__version__ = "0.1.0"
+__author__ = 'Blaž Šrklj, Sašo Đerovski, Urh Primožič, Matej Petkovič'
+__credits__ = 'Jozef Stefan Institute'
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='frane',
+    version='0.1.0',    
+    description='Unsupervised Feature Ranking via Attribute Networks',
+    url='https://github.com/FRANe-team/FRANe',
+    author='Blaž Šrklj, Sašo Đerovski, Urh Primožič, Matej Petkovič',
+    author_email='urh.primozic@student.fmf.uni-lj.si',
+    license='BSD 2-clause',
+    packages=['frane'],
+    install_requires=['numpy>=1.19.5',
+                      'scipy>=1.6.0',                     
+                      ],
+    project_urls={
+        "Bug Tracker": "https://github.com/FRANe-team/FRANe/issues",
+        "Article" : "not-yet-published",
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)

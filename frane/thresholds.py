4a368fef5bcf31d9e81fcd5dab9d0d814e865eb4
urh <urh.primozic@gmail.com>
2021-08-10 13:20:41 +0200

    cleanup

--- frane/thresholds.py
+++ frane/thresholds.py
@@ -1,29 +1,4 @@
 import numpy as np
-# TODO to je grdo grdo grdop
-
-
-def constant_number(start, stop, num, distances, sorted_indexes):
-    """
-    Returns thresholds, where number of distances between every threshold is the same.
-
-    Returns
-    -------
-    tresholds : list
-    """
-    shape = distances.shape[0]  # number of features
-    # add the same number of distances every time
-    step = (shape**2) // num
-    threshold = [
-        distances[sorted_indexes[index] // shape,
-                  sorted_indexes[index] % shape]
-        for index in range(step - 1, shape**2, step)
-    ]
-    # plus the end
-    if threshold[-1] != distances[sorted_indexes[-1] // shape,
-                                  sorted_indexes[-1] % shape]:
-        threshold.append(distances[sorted_indexes[-1] // shape,
-                                   sorted_indexes[-1] % shape])
-    return threshold
 
 
 threshold_dict = {
@@ -41,7 +16,5 @@ threshold_dict = {
         start, np.median(distances, axis=None), num),
     'mean_linear':
     lambda start, stop, num, distances, sorted_indexes: np.linspace(
-        start, np.mean(distances, axis=None), num),
-    'constant_number':
-    constant_number
+        start, np.mean(distances, axis=None), num)
 }

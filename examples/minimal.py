57c5233a2e3e3a2c12a60523944e23078a58349c
urh <urh.primozic@gmail.com>
2021-08-10 12:25:14 +0200

    Pa tko, da se vidi, kako se podatke prebere.

--- examples/minimal.py
+++ examples/minimal.py
@@ -1,8 +1,11 @@
 from frane import FRANe, load_data
+from scipy.io import loadmat
 import numpy as np
 
-x = np.random.random((100, 1000))
-x#  = load_data('../data/lung_small.mat') # data from https://github.com/jundongl/scikit-feature
+#x = np.random.random((100, 1000))
+
+# data from https://github.com/jundongl/scikit-feature
+x = loadmat('../data/lung_small.mat')["X"].astype(np.float32)
 r = FRANe()
 r.fit(x)
 scores = r.feature_importances_
